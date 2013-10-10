8df58144e3382eaa378904c2ab5005582cb1e084
Takahashi Keijiro <keijiro@supermsx.jt.unity3d.com>
2013-10-10 11:16:06 +0900

    Initial commit.

---
+++ Assets/Editor/AssetBundleExporter.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+public class AssetBundleExporter
+{
+    [MenuItem("Assets/Build AssetBundle From Selection")]
+    static void BuildAssetBundleFromSelection ()
+    {
+        string path = EditorUtility.SaveFilePanel ("Save Asset Bundle", "", "assets", "unity3d");
+        if (path.Length != 0) {
+            Object[] selection = Selection.GetFiltered (typeof(Object), SelectionMode.DeepAssets);
+            BuildPipeline.BuildAssetBundle (Selection.activeObject, selection, path, BuildAssetBundleOptions.CollectDependencies | BuildAssetBundleOptions.CompleteAssets, CurrentTargetPlatform);
+            Selection.objects = selection;
+        }
+    }
+
+    static BuildTarget CurrentTargetPlatform {
+        get {
+#if UNITY_IPHONE
+            return BuildTarget.iPhone;
+#elif UNITY_ANDROID
+            return BuildTarget.Android;
+#else
+            return BuildTarget.WebPlayer;
+#endif
+        }
+    }
+}
---
+++ Assets/MyCustomShader.shader
@@ -0,0 +1,27 @@
+﻿Shader "Custom/MyCustomShader" {
+	Properties {
+		_MainTex ("Base (RGB)", 2D) = "white" {}
+		_Color ("Color", Color) = (1, 1, 1, 1)
+	}
+	SubShader {
+        Tags { "Queue" = "Transparent" }
+		
+		CGPROGRAM
+		#pragma surface surf Lambert alpha
+
+		sampler2D _MainTex;
+		float4 _Color;
+
+		struct Input {
+			float2 uv_MainTex;
+		};
+
+		void surf (Input IN, inout SurfaceOutput o) {
+			half4 c = tex2D (_MainTex, IN.uv_MainTex);
+			o.Albedo = c.rgb * _Color.rgb;
+			o.Alpha = c.a * _Color.a;
+		}
+		ENDCG
+	} 
+	FallBack Off
+}
